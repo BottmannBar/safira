e448c75580749cb159f6fa66c1a70ff05ee74c30
BottmannBar <153724835+BottmannBar@users.noreply.github.com>
2026-07-03 17:20:10 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -39,7 +39,7 @@ async function apiPost(modulo, dados) {
     const response = await fetch(CONFIG.API_URL, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'text/plain;charset=utf-8'
       },
       body: JSON.stringify(payload)
     });
